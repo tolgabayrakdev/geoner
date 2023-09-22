fe4b5519f4f4441ad0e0e772b5c48bd04b9dbabb
tolgabayrakdev <bayraktolga28@gmail.com>
2023-09-22 08:18:20 +0300

    auth user depends

--- server/app/depends/authentication_user.py
+++ server/app/depends/authentication_user.py
@@ -4,11 +4,9 @@ import jwt
 
 def authentication_user(request: Request):
     try:
-
         access_token = request.cookies.get("access_token")
         if access_token:
             decoded_token = jwt.decode(access_token, "secret", algorithms=["HS256"])
-            print(decoded_token["payload"])
             return decoded_token["payload"]
         else:
             return None
