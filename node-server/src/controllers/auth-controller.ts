b743258dbf6a5176bb09a9184f4057c7c6d753d0
tolgabayrakdev <bayraktolga28@gmail.com>
2023-10-06 21:14:21 +0300

    helper class

---
+++ node-server/src/controllers/auth-controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+
+export class AuthController {
+
+    public login = async (req: Request, res: Response): Promise<void> => {
+        try {
+            
+        } catch (error: any) {
+            throw new Error;
+        }
+    }
+}
---
+++ node-server/src/utils/helper.ts
@@ -0,0 +1,32 @@
+import Crypto from "crypto";
+import Jwt from "jsonwebtoken";
+
+export class Helper {
+
+    /**
+     * generateAccessToken
+     */
+    public generateAccessToken(payload: object): string {
+        return Jwt.sign(payload, "SECRET_KEY",{
+            expiresIn: "1h",
+        });
+    }
+
+    /**
+     * generateRefreshToken
+     */
+    public generateRefreshToken(payload: object): string {
+        return Jwt.sign(payload, "SECRET_KEY", {
+            expiresIn: "7d",
+        });
+    }
+
+    /**
+     * generateHashPassword
+     */
+    public generateHashPassword(password: string): string {
+        return Crypto.createHash("sha256").update(password).digest("base64");
+    }
+
+    
+}
