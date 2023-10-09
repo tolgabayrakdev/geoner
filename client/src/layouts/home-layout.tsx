26d2b434f5bf71ec06bfcedc35f63aaebbf0c569
tolgabayrakdev <bayraktolga28@gmail.com>
2023-10-09 10:05:03 +0300

    remove vitejs react project

--- client/src/layouts/home-layout.tsx
+++
@@ -1,5 +0,0 @@
-type Props = {};
-
-export default function HomeLayout({}: Props) {
-  return <div>home-layout</div>;
-}
--- client/src/pages/main/profile.tsx
+++
@@ -1,5 +0,0 @@
-type Props = {};
-
-export default function Profile({}: Props) {
-  return <div>profile</div>;
-}
